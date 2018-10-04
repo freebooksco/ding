d43e5356a9e36989d0612c60dd2c9f996cfda6f9
freebooksco <freebooksco@gmail.com>
2018-10-04 22:39:37 +0530

    Update morelink.js

--- morelink.js
+++ morelink.js
@@ -1,4 +1,7 @@
 [
+{text:'Home',link:'https://www.44books.com' },
+{text:'AllBooks',link:'https://www.44books.com/p/free-hindi-books.html' },
+{text:'Categories',link:'https://www.44books.com/p/download-hindi-books-pdf.html' },
 {text:'Upanyas',link:'https://www.44books.com/search/label/upanyas' },
 {text:'Religious',link:'https://www.44books.com/search/label/dharmik' },
 ]
